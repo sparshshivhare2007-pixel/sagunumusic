082606c4cb9e1b5786583e616205ab39d3ee0e12
⏤͟͞ 𝙎𝙋𝘼𝙍𝙎𝙃 𝘽𝘼𝙉𝙄𝙔𝘼 <sparshshivhare2007@gmail.com>
2025-12-11 23:17:52 +0530

    Update config.py

--- config.py
+++ config.py
@@ -55,7 +55,7 @@ HEROKU_API_KEY = os.getenv("HEROKU_API_KEY")
 # 🔄 Git & Update Settings
 # ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
 
-UPSTREAM_REPO = os.getenv("UPSTREAM_REPO", "https://github.com/NoxxOP/ShrutiMusic")
+UPSTREAM_REPO = os.getenv("UPSTREAM_REPO", "https://github.com/sparshshivhare2007-pixel/sagunumusic")
 UPSTREAM_BRANCH = os.getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = os.getenv("GIT_TOKEN", None)
 
@@ -63,12 +63,12 @@ GIT_TOKEN = os.getenv("GIT_TOKEN", None)
 # 🔗 Support Links
 # ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
 
-SUPPORT_CHANNEL = os.getenv("SUPPORT_CHANNEL", "https://t.me/ShrutiBots")
-SUPPORT_GROUP = os.getenv("SUPPORT_GROUP", "https://t.me/ShrutiBotSupport")
-INSTAGRAM = os.getenv("INSTAGRAM", "https://instagram.com/yaduwanshi_nand")
-YOUTUBE = os.getenv("YOUTUBE", "https://youtube.com/@NandEditz")
-GITHUB = os.getenv("GITHUB", "https://github.com/NoxxOP")
-DONATE = os.getenv("DONATE", "https://t.me/ShrutiBots/91")
+SUPPORT_CHANNEL = os.getenv("SUPPORT_CHANNEL", "https://t.me/internalxfeelings)
+SUPPORT_GROUP = os.getenv("SUPPORT_GROUP", "https://t.me/Sanginixbots")
+INSTAGRAM = os.getenv("INSTAGRAM", "https://t.me/Sanginixbots")
+YOUTUBE = os.getenv("YOUTUBE", "https://t.me/Sanginixbots")
+GITHUB = os.getenv("GITHUB", "https://t.me/Sanginixbots")
+DONATE = os.getenv("DONATE", "https://t.me/Sanginixbots")
 PRIVACY_LINK = os.getenv("PRIVACY_LINK", "https://graph.org/Privacy-Policy-05-01-30")
 
 # ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
@@ -112,18 +112,18 @@ AUTO_LEAVING_ASSISTANT = bool(os.getenv("AUTO_LEAVING_ASSISTANT", False))
 # 🖼️ Image URLs (Can be customized)
 # ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
 
-START_IMG_URL = os.getenv("START_IMG_URL", "https://files.catbox.moe/7q8bfg.jpg")
-PING_IMG_URL = "https://files.catbox.moe/eehxb4.jpg"
-PLAYLIST_IMG_URL = "https://files.catbox.moe/eehxb4.jpg"
-STATS_IMG_URL = "https://files.catbox.moe/eehxb4.jpg"
-TELEGRAM_AUDIO_URL = "https://files.catbox.moe/eehxb4.jpg"
-TELEGRAM_VIDEO_URL = "https://files.catbox.moe/eehxb4.jpg"
-STREAM_IMG_URL = "https://files.catbox.moe/eehxb4.jpg"
-SOUNCLOUD_IMG_URL = "https://files.catbox.moe/eehxb4.jpg"
-YOUTUBE_IMG_URL = "https://files.catbox.moe/eehxb4.jpg"
-SPOTIFY_ARTIST_IMG_URL = "https://files.catbox.moe/eehxb4.jpg"
-SPOTIFY_ALBUM_IMG_URL = "https://files.catbox.moe/eehxb4.jpg"
-SPOTIFY_PLAYLIST_IMG_URL = "https://files.catbox.moe/eehxb4.jpg"
+START_IMG_URL = os.getenv("START_IMG_URL", "https://files.catbox.moe/jnr858.jpg")
+PING_IMG_URL = "https://files.catbox.moe/jnr858.jpg"
+PLAYLIST_IMG_URL = "https://files.catbox.moe/jnr858.jpg"
+STATS_IMG_URL = "https://files.catbox.moe/jnr858.jpg"
+TELEGRAM_AUDIO_URL = "https://files.catbox.moe/jnr858.jpg"
+TELEGRAM_VIDEO_URL = "https://files.catbox.moe/jnr858.jpg"
+STREAM_IMG_URL = "https://files.catbox.moe/jnr858.jpg"
+SOUNCLOUD_IMG_URL = "https://files.catbox.moe/jnr858.jpg"
+YOUTUBE_IMG_URL = "https://files.catbox.moe/jnr858.jpg"
+SPOTIFY_ARTIST_IMG_URL = "https://files.catbox.moe/jnr858.jpg"
+SPOTIFY_ALBUM_IMG_URL = "https://files.catbox.moe/jnr858.jpg"
+SPOTIFY_PLAYLIST_IMG_URL = "https://files.catbox.moe/jnr858.jpg"
 
 # ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
 # 🔐 User & Bot State Stores
