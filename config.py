74562825ee4f1aad8cc1fa8f9f6d696a922fb376
⏤͟͞ 𝙎𝙋𝘼𝙍𝙎𝙃 𝘽𝘼𝙉𝙄𝙔𝘼 <sparshshivhare2007@gmail.com>
2025-12-16 11:14:09 +0530

    Update config.py

--- config.py
+++ config.py
@@ -39,8 +39,8 @@ API_ID = int(os.getenv("API_ID"))
 API_HASH = os.getenv("API_HASH")
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 OWNER_ID = int(os.getenv("OWNER_ID", None))
-OWNER_USERNAME = os.getenv("OWNER_USERNAME", "ll_ihodophile_ll")
-BOT_USERNAME = os.getenv("BOT_USERNAME", "Sreexmusicbot")
+OWNER_USERNAME = os.getenv("OWNER_USERNAME", "Maayavi_hubhai")
+BOT_USERNAME = os.getenv("BOT_USERNAME", "DestinyXmusicbot")
 
 # ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
 # 🛠️ Database & Deployment Configs
